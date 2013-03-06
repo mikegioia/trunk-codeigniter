4a8e0565aad524578dec97572dca9bdbf144b8e2
Mike Gioia <mikegioia@gmail.com>
2013-03-05 20:58:40 -0500

    adding config loading hook because we can't call helper functions from the config file...

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -10,7 +10,12 @@
 |
 */
 
-
+$hook[ 'pre_controller' ] = array(
+    'class' => 'Utility',
+    'function' => 'load_config',
+    'filename' => 'Utility.php',
+    'filepath' => 'hooks',
+    'params' => array() );
 
 /* End of file hooks.php */
 /* Location: ./application/config/hooks.php */
